8d69d1607a45d1920237d138161424aa4f6528f0
TheChilledBuffalo <abhishekgs.tvm@gmail.com>
2024-09-06 15:51:17 +0530

    Initial Commit

---
+++ app/layout.tsx
@@ -0,0 +1,35 @@
+import type { Metadata } from "next";
+import localFont from "next/font/local";
+import "./globals.css";
+
+const geistSans = localFont({
+  src: "./fonts/GeistVF.woff",
+  variable: "--font-geist-sans",
+  weight: "100 900",
+});
+const geistMono = localFont({
+  src: "./fonts/GeistMonoVF.woff",
+  variable: "--font-geist-mono",
+  weight: "100 900",
+});
+
+export const metadata: Metadata = {
+  title: "Syntax Sangam",
+  description: "A fun and innovative way to learn coding!",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased w-full h-screen`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
