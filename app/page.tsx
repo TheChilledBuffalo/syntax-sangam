a21aad29163b2c80303c62ef59a1ba0473df7b89
TheChilledBuffalo <abhishekgs.tvm@gmail.com>
2024-09-07 12:34:37 +0530

    feat: add home page design

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,27 @@
+import SparklesText from "@/components/magicui/sparkles-text";
+import { Highlight } from "@/components/ui/hero-highlight";
+import Particles from "@/components/magicui/particles";
+
 export default function Home() {
   return (
-    <div className="w-full h-full flex items-center justify-center text-4xl">
-      Syntax Sangam
-    </div>
+    <>
+      <div className="w-full h-full flex flex-col items-center justify-center text-5xl gap-y-7 font-semibold
+    animate-in fade-in slide-in-from-top-3 duration-1000 text-balance bg-gradient-to-br from-black from-30% to-black/40
+    bg-clip-text pb-2 leading-none text-transparent dark:from-white dark:to-white/80 select-none">
+        <div className={""}>
+          Learn Coding
+        </div>
+        <div className={"flex flex-row items-center justify-center gap-x-3"}>
+          The <SparklesText text={"Smart"} className={"text-6xl text-black dark:text-white"} sparklesCount={7}/> Way
+        </div>
+        <div>
+          With
+          <Highlight className={"text-black dark:text-white bg-opacity-70 ml-2"}>
+            Syntax Sangam
+          </Highlight>
+        </div>
+      </div>
+      <Particles className={"fixed z-[-10] w-full h-full inset-0"} />
+    </>
   )
 }
